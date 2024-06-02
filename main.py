b0b77358c4f0c87d1bbfc2b823f96d5f26703be3
just4066 <justin.laroche3@usherbrooke.ca>
2024-06-02 15:01:39 -0400

    Add gitignore and add read_book, count_words, count_caracters, sort_caracters & print_report functions.

---
+++ main.py
@@ -0,0 +1,44 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    print_report(book_path) 
+           
+def read_book(book_path):
+    with open (book_path) as f:
+        return f.read()
+            
+def count_words(files_contents):
+    words = files_contents.split()
+    return len(words)
+
+def count_caracters(text):
+    list_of_words = text.split()
+    dict_of_caracters = {}
+    
+    for word in list_of_words:
+        if word.isalpha() == True:
+            lowered_word = word.lower()
+            
+            for caracter in lowered_word:
+                if caracter in dict_of_caracters:
+                    dict_of_caracters[caracter] += 1
+                else:
+                    dict_of_caracters[caracter] = 1
+
+    return dict_of_caracters
+
+def sort_caracters(caracters_count):
+    return sorted(caracters_count.items(), key=lambda x: x[1], reverse=True)
+
+def print_report(text_path):
+    text = read_book(text_path)
+    words_count = count_words(text)
+    caracters_count = count_caracters(text)
+    sorted_caracters = sort_caracters(caracters_count)
+    
+    print(f"--- Begin report of {text_path} ---")
+    print(f"{words_count} words in the document\n\n")
+    
+    for caracter, count in sorted_caracters:
+      print(f"The caracter '{caracter}' was found {count} times")
+      
+main()
